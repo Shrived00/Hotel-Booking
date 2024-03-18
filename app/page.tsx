ccf405069983020d65d0e8fb77411bc33c798ba1
Shrived00 <shrivedkakde@gmail.com>
2024-03-18 14:49:25 +0530

    Done

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,18 @@
 import getCurrentUser from "./actions/getCurrentUser";
-import getListing from "./actions/getListings";
+import getListings, {
+  IListingsParams,
+} from "@/app/actions/getListings";
 import ClientOnly from "./components/ClientOnly";
 import Container from "./components/Container";
 import EmptyState from "./components/EmptyState";
 import ListingCard from "./components/listings/ListingCard";
 
-export default async function Home() {
+interface HomeProps {
+  searchParams: IListingsParams
+}
 
-  const listings = await getListing();
+const Home = async ({ searchParams }: HomeProps) => {
+  const listings = await getListings(searchParams);
   const currentUser = await getCurrentUser();
 
   if (listings.length == 0) {
@@ -19,7 +24,6 @@ export default async function Home() {
       </ClientOnly>
     );
   }
-
   return (
     <ClientOnly>
       <Container>
@@ -41,3 +45,6 @@ export default async function Home() {
 
   );
 }
+
+
+export default Home;
